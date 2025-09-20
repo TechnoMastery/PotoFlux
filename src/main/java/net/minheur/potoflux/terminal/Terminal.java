3cd8b11e00fc7c3f1e9854287cb6f995f4bdad78
Minheur2000 <adrien.rousselet@hotmail.com>
2025-09-20 17:29:58 +0200

    added a settings button

--- src/main/java/net/minheur/potoflux/terminal/Terminal.java
+++ src/main/java/net/minheur/potoflux/terminal/Terminal.java
@@ -38,12 +38,58 @@ public class Terminal {
         JLabel prompt = new JLabel("  > " );
         prompt.setFont(new Font("Consolas", Font.PLAIN, 20));
         inputField.setFont(new Font("Consolas", Font.PLAIN, 20));
-
         inputPanel.add(prompt, BorderLayout.WEST);
         inputPanel.add(inputField, BorderLayout.CENTER);
 
+        // toolbar
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+
+        // create settings button
+        JButton settingsButton = new JButton("⚙");
+        settingsButton.addActionListener(e -> {
+            // create window -basic-
+            JDialog dialog = new JDialog(frame, "Settings", true);
+            dialog.setSize(300, 150);
+            dialog.setLayout(new BorderLayout());
+            dialog.setLocationRelativeTo(frame);
+            // font size -setting-
+            JTextField fontSizeParameterField = new JTextField();
+            fontSizeParameterField.setText(String.valueOf(outputArea.getFont().getSize()));
+            // apply button -base-
+            JButton applyButton = new JButton("Apply");
+            applyButton.addActionListener(ev -> {
+                try {
+                    int newSize = Integer.parseInt(fontSizeParameterField.getText().trim());
+                    // check font not to small
+                    if (newSize > 5) {
+                        Font newFont = outputArea.getFont().deriveFont(((float) newSize));
+                        outputArea.setFont(newFont);
+                        inputField.setFont(newFont);
+                    }
+                    // close window
+                    dialog.dispose();
+                } catch (NumberFormatException exception) {
+                    JOptionPane.showMessageDialog(dialog, "Please input a valid number !");
+                }
+            });
+            // settings pane
+            JPanel panel = new JPanel(new BorderLayout(5, 5));
+            panel.add(new JLabel("Font size :"), BorderLayout.NORTH);
+            panel.add(fontSizeParameterField, BorderLayout.CENTER);
+            panel.add(applyButton, BorderLayout.SOUTH);
+
+            dialog.add(panel, BorderLayout.CENTER);
+            dialog.setVisible(true);
+        });
+
+        // adding buttons
+        toolBar.add(settingsButton);
+
+        // main adding to frame
         frame.add(inputPanel, BorderLayout.SOUTH);
         frame.getContentPane().add(scrollPanel, BorderLayout.CENTER);
+        frame.add(toolBar, BorderLayout.NORTH);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
