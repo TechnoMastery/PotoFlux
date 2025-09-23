a5309755750ff1a4c9ef813e0a01df254ab64e5b
Minheur2000 <adrien.rousselet@hotmail.com>
2025-09-23 19:07:42 +0200

    added json lang system

--- src/main/java/net/minheur/potoflux/PotoFlux.java
+++ src/main/java/net/minheur/potoflux/PotoFlux.java
@@ -1,6 +1,7 @@
 package net.minheur.potoflux;
 
 import net.minheur.potoflux.screen.PotoScreen;
+import net.minheur.potoflux.translations.Translations;
 
 import javax.swing.*;
 
@@ -8,7 +9,8 @@ public class PotoFlux {
     public static PotoScreen app;
 
     public static void main(String[] args) {
-
+        Translations.load("en");
+        Translations.load("fr");
         SwingUtilities.invokeLater(() -> {
             app = new PotoScreen();
         });
---
+++ src/main/java/net/minheur/potoflux/translations/Translations.java
@@ -0,0 +1,32 @@
+package net.minheur.potoflux.translations;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import javax.swing.*;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translations {
+    private static Map<String, String> translations = new HashMap<>();
+
+    public static void load(String langCode) {
+        try (Reader reader = new InputStreamReader(
+                Translations.class.getResourceAsStream("/lang/" + langCode + ".json"),
+                StandardCharsets.UTF_8
+        )) {
+            translations = new Gson().fromJson(reader, new TypeToken<Map<String, String>>(){}.getType());
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "ERROR: Failed loading translations ! Please report this error.");
+            translations = new HashMap<>();
+        }
+    }
+
+    public static String get(String key) {
+        return translations.getOrDefault(key, key);
+    }
+}
