cd2e7ac497478df6dce71bd618d088329ee96308
Minheur2000 <adrien.rousselet@hotmail.com>
2025-10-15 17:18:27 +0200

    patch : missing todo

--- src/main/java/net/minheur/potoflux/screen/tabs/all/TodoTab.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/all/TodoTab.java
@@ -223,7 +223,7 @@ public class TodoTab extends BaseTab {
         content.add(Box.createVerticalStrut(10));
 
         // pine checkbox
-        JCheckBox pineCheck = new JCheckBox(Translations.get("tabs.todo.pined"));
+        JCheckBox pineCheck = new JCheckBox(Translations.get("tabs.todo.pinned")); // TODO
         pineCheck.setSelected(todo.pinned);
         pineCheck.setAlignmentX(Component.CENTER_ALIGNMENT);
         content.add(pineCheck);
