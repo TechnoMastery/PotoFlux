904aa66c097ffe0a477aa58cbaad38c2c86d327d
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-21 13:17:04 +0100

    rollback some changes (bis)

--- src/main/java/net/minheur/potoflux/screen/tabs/Tabs.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/Tabs.java
@@ -4,7 +4,6 @@ import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import net.minheur.potoflux.registry.RegistryList;
 import net.minheur.potoflux.screen.tabs.all.*;
 import net.minheur.potoflux.translations.Translations;
-import net.minheur.potoflux.utils.ressourcelocation.ResourceLocation;
 
 import static net.minheur.potoflux.PotoFlux.fromModId;
 
@@ -19,26 +18,13 @@ public class Tabs {
         hasGenerated = true;
     }
 
-    // public static final ResourceLocation HOME = fromModId("home");
-    // public static final ResourceLocation TERMINAL = fromModId("terminal");
-    // public static final ResourceLocation CARD_LEARN = fromModId("card_learn");
-    // public static final ResourceLocation SETTINGS = fromModId("settings");
-
     public final Tab HOME = LIST.add(new Tab(fromModId("home"), Translations.get("potoflux:tabs.home.name"), HomeTab.class));
     public final Tab TERMINAL = LIST.add(new Tab(fromModId("terminal"), Translations.get("potoflux:tabs.term.name"), TerminalTab.class));
     public final Tab CARD_LEARN = LIST.add(new Tab(fromModId("card_learn"), Translations.get("potoflux:tabs.card.name"), CardLearningTab.class));
     public final Tab SETTINGS = LIST.add(new Tab(fromModId("settings"), Translations.get("potoflux:tabs.settings.title"), SettingsTab.class));
 
-    private void createTabs() {
-        // LIST.add(new Tab(HOME, Translations.get("potoflux:tabs.home.name"), HomeTab.class));
-        // LIST.add(new Tab(TERMINAL, Translations.get("potoflux:tabs.term.name"), TerminalTab.class));
-        // LIST.add(new Tab(CARD_LEARN, Translations.get("potoflux:tabs.card.name"), CardLearningTab.class));
-        // LIST.add(new Tab(SETTINGS, Translations.get("potoflux:tabs.settings.title"), SettingsTab.class));
-    }
-
     public static void register(RegisterTabsEvent event) {
         INSTANCE = new Tabs();
-        INSTANCE.createTabs();
 
         INSTANCE.LIST.register(event.reg);
     }
