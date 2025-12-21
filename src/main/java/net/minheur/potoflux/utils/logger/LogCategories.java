4b487c3d4416945d3b2e6429e175de4d5bda6e6a
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-21 11:05:41 +0100

    added log category

--- src/main/java/net/minheur/potoflux/utils/logger/LogCategories.java
+++ src/main/java/net/minheur/potoflux/utils/logger/LogCategories.java
@@ -3,7 +3,8 @@ package net.minheur.potoflux.utils.logger;
 public enum LogCategories implements ILogCategory {
     TERMINAL("terminal"),
     TRANSLATIONS("translations"),
-    MOD_LOADER("modLoader");
+    MOD_LOADER("modLoader"),
+    SCREEN("screen");
 
     private final String code;
 
