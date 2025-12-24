ab5438a13cabfbe139cd4bea56b52e5294bd3d68
Minheur4000 <ordi2526883@educ.normandie.fr>
2025-12-24 21:17:16 +0100

    worked

--- src/main/java/net/minheur/potoflux/catalog/CatalogGetterHandler.java
+++ src/main/java/net/minheur/potoflux/catalog/CatalogGetterHandler.java
@@ -3,19 +3,23 @@ package net.minheur.potoflux.catalog;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import net.minheur.potoflux.utils.Json;
 import net.minheur.potoflux.utils.OnlineReader;
 import net.minheur.potoflux.utils.logger.LogCategories;
 import net.minheur.potoflux.utils.logger.PtfLogger;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CatalogGetterHandler {
     private static final Gson GSON = Json.GSON;
     private static final String baseDir = "https://technomastery.github.io/PotoFluxAppData/modCatalog/";
 
-    private static final Map<String, JsonObject> catalog = new HashMap<>();
+    private static final Map<String, ModCatalog> catalog = new HashMap<>();
 
     public static void buildCatalog() {
         String mainCatalogPath = baseDir + "mainCatalog.json";
@@ -27,5 +31,27 @@ public class CatalogGetterHandler {
         }
 
         JsonArray mainCatalogJson = GSON.fromJson(mainCatalogContent, JsonArray.class);
+        List<String> allMods = new ArrayList<>();
+
+        mainCatalogJson.forEach(j -> allMods.add(j.getAsString()));
+
+        for (String modId : allMods) {
+            String modCatalogPath = baseDir + modId + "/" + modId + ".json";
+
+            String modCatalogContent = OnlineReader.read(modCatalogPath);
+            if (modCatalogContent == null) {
+                PtfLogger.error("Failed to read catalog for '" + modId + "' ! Skipping...", LogCategories.CATALOG);
+                continue;
+            }
+
+            Type typeToken = new TypeToken<ModCatalog>() {}.getType();
+            ModCatalog modCatalog = GSON.fromJson(modCatalogContent, typeToken);
+
+            catalog.put(modId, modCatalog);
+        }
+    }
+
+    public static ModCatalog getCatalog(String modId) {
+        return catalog.get(modId);
     }
 }
---
+++ src/main/java/net/minheur/potoflux/catalog/ModCatalog.java
@@ -0,0 +1,14 @@
+package net.minheur.potoflux.catalog;
+
+import java.util.Map;
+
+public class ModCatalog {
+    public String modId;
+    public boolean isPublished;
+    public Map<String, ModVersion> versions;
+
+    public static class ModVersion {
+        public String fileName;
+        public boolean isPublished;
+    }
+}
