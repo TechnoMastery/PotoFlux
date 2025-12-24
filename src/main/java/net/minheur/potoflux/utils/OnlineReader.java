ffbffff57b29000fe662c765670fdbcbfa10bf84
Minheur4000 <ordi2526883@educ.normandie.fr>
2025-12-24 19:18:56 +0100

    updated OnlineReader

--- src/main/java/net/minheur/potoflux/utils/OnlineReader.java
+++ src/main/java/net/minheur/potoflux/utils/OnlineReader.java
@@ -1,24 +1,32 @@
 package net.minheur.potoflux.utils;
 
+import net.minheur.potoflux.utils.logger.PtfLogger;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class OnlineReader {
-    public static String read(String url) throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
+    public static String read(String url) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
 
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        if (response.statusCode() != 200) throw new RuntimeException("HTTP " + response.statusCode());
+            if (response.statusCode() != 200) throw new RuntimeException("HTTP " + response.statusCode());
 
-        return response.body();
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
