b3e6b4cb8578b9c920f670ca58fa6c2b0827de59
Minheur4000 <ordi2526883@educ.normandie.fr>
2026-01-15 13:57:41 +0100

    removed useless imports

--- src/main/java/net/minheur/potoflux/catalog/CatalogHelpers.java
+++ src/main/java/net/minheur/potoflux/catalog/CatalogHelpers.java
@@ -18,7 +18,6 @@ import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class CatalogHelpers {
