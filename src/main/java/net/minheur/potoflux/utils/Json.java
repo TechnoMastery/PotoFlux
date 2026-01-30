85209f98e850db9dc5e45fb0b6b1fd069d1af468
Minheur2000 <adrien.rousselet@hotmail.com>
2026-01-30 19:10:05 +0100

    added utility Json

--- src/main/java/net/minheur/potoflux/utils/Json.java
+++ src/main/java/net/minheur/potoflux/utils/Json.java
@@ -1,6 +1,8 @@
 package net.minheur.potoflux.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.InputStreamReader;
@@ -21,6 +23,18 @@ public class Json {
             Type type = new TypeToken<List<String>>(){}.getType();
 
             return GSON.fromJson(reader, type);
+        }
+    }
+
+    public static String getFromObject(URL url, String key) throws Exception {
+        try (InputStreamReader reader =
+                     new InputStreamReader(url.openStream(), StandardCharsets.UTF_8)) {
+
+            Type type = new TypeToken<List<String>>(){}.getType();
+
+            JsonObject object = JsonParser.parseReader(reader).getAsJsonObject();
+
+            return object.get(key).toString();
         }
     }
 }
