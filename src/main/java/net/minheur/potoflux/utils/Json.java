f89ebae441c0536f2d25316f1e02a4ec0207b09f
Minheur2000 <adrien.rousselet@hotmail.com>
2026-01-30 17:42:04 +0100

    added json helper method

--- src/main/java/net/minheur/potoflux/utils/Json.java
+++ src/main/java/net/minheur/potoflux/utils/Json.java
@@ -1,9 +1,26 @@
 package net.minheur.potoflux.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Json {
     private Json() {}
 
     public static final Gson GSON = new Gson();
+
+    public static List<String> loadStringArray(URL url) throws Exception {
+        try (InputStreamReader reader =
+                new InputStreamReader(url.openStream(), StandardCharsets.UTF_8)) {
+
+            Type type = new TypeToken<List<String>>(){}.getType();
+
+            return GSON.fromJson(reader, type);
+        }
+    }
 }
