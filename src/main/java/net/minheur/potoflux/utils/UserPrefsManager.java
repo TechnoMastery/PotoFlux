cc5efb5626870fc3361a154468dfc6deefaf10ad
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-01 13:02:25 +0100

    did javaDoc for the UserPrefsManager

--- src/main/java/net/minheur/potoflux/utils/UserPrefsManager.java
+++ src/main/java/net/minheur/potoflux/utils/UserPrefsManager.java
@@ -9,30 +9,67 @@ import javax.swing.*;
 import java.util.Arrays;
 import java.util.prefs.Preferences;
 
+/**
+ * This class is used to store and retrieve user preferences.
+ */
 public class UserPrefsManager {
+    /**
+     * Actual prefs var for the user
+     */
     private static final Preferences prefs = Preferences.userNodeForPackage(UserPrefsManager.class);
+
     // lang
+    /**
+     * Key for storing the user language
+     */
     private static final String KEY_LANG = "user_lang";
+    /**
+     * All values that the language var can take
+     */
     private static final String[] langOptions = new String[Lang.values().length];
     static {
         for (int i = 0; i < Lang.values().length; i++) langOptions[i] = Lang.values()[i].code;
     }
 
     // ascii
+    /**
+     * Key for storing the user's terminal ASCII
+     */
     private static final String KEY_ASCII = "terminal_ascii";
+    /**
+     * All values the user's terminal ASCII can take
+     */
     private static final String[] asciiOptions = { "basic", "big", "chiseled" };
 
     // theme
+    /**
+     * Key for storing the user's theme
+     */
     private static final String KEY_THEME = "app_theme";
+    /**
+     * All values the user's theme can take
+     */
     private static final String[] themeOptions = { "dark", "light" };
 
     // getters
+    /**
+     * Getter for the terminal's ASCII
+     * @return the terminal's ASCII
+     */
     public static String getTerminalASCII() {
         return prefs.get(KEY_ASCII, null);
     }
+    /**
+     * Getter for the user lang (direct)
+     * @return the user lang
+     */
     public static String getStrictUserLang() {
         return prefs.get(KEY_LANG, null);
     }
+    /**
+     * Getter for the user lang (asking if null)
+     * @return the user lang or asks the user for it
+     */
     public static String getUserLang() {
         String lang = getStrictUserLang();
         if (lang == null) {
@@ -41,11 +78,19 @@ public class UserPrefsManager {
         }
         return lang;
     }
+    /**
+     * Getter for the user's theme
+     * @return the user's theme
+     */
     public static String getTheme() {
         return prefs.get(KEY_THEME, "light");
     }
 
     // askers
+    /**
+     * Asks the user for the lang
+     * @return the lang chosen
+     */
     private static String askUserLang() {
         int actualLangId = 0;
         String strictLang = getStrictUserLang();
@@ -64,6 +109,10 @@ public class UserPrefsManager {
         return lang;
     }
 
+    /**
+     * Asks the user for the terminal's ASCII
+     * @return the chosen ASCII
+     */
     private static String askUserAscii() {
         int actualAsciiId = 0;
         String strictAscii = getTerminalASCII();
@@ -82,6 +131,10 @@ public class UserPrefsManager {
         return ascii;
     }
 
+    /**
+     * Asks the user for the theme
+     * @return the chosen theme
+     */
     private static String askUserTheme() {
         String actualTheme = getTheme();
         int actualThemeId = Arrays.asList(themeOptions).indexOf(actualTheme);
@@ -101,23 +154,35 @@ public class UserPrefsManager {
     }
 
 
-    // reseters
+    // resets
+    /**
+     * Runs the reset for the user lang
+     */
     public static void resetUserLang() {
         prefs.put(KEY_LANG, askUserLang());
         showReload();
     }
 
+    /**
+     * Runs the reset for the terminal's ASCII
+     */
     public static void resetTerminalAscii() {
         prefs.put(KEY_ASCII, askUserAscii());
         CommandProcessor.clearArea();
         showReload();
     }
 
+    /**
+     * Runs the reset for the theme
+     */
     public static void resetTheme() {
         prefs.put(KEY_THEME, askUserTheme());
         showReload();
     }
 
+    /**
+     * Shows a popup to ask the user to restart the app
+     */
     private static void showReload() {
         JOptionPane.showMessageDialog(null, Translations.get("potoflux:prefs.reload"));
         PotoFlux.runProgramClosing(0);
