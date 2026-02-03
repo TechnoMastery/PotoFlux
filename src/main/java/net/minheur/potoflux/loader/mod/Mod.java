d7fb30dec08925c43d4a9ac6774b8e6b52de6454
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-03 19:28:26 +0100

    worked on mod loading classes javaDoc

--- src/main/java/net/minheur/potoflux/loader/mod/Mod.java
+++ src/main/java/net/minheur/potoflux/loader/mod/Mod.java
@@ -6,10 +6,27 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.List;
 
+/**
+ * The class to annotate your main mod class with.<br>
+ * This contains all main data for the mod (that doesn't need to create the mod to be accessed).
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Mod {
+    /**
+     * The mod ID of your mod. Should be a unique identifier for your mod.
+     * @return your mod's ID
+     */
     String modId();
+    /**
+     * The version of your mod.
+     * @return your mod's version
+     */
     String version();
+    /**
+     * The list of all potoflux versions your mod is compatible with.<br>
+     * Should take the form of {@code compatibleVersions = {"6.0", "6.1"}}
+     * @return potoflux versions compatible with your mod
+     */
     String[] compatibleVersions();
 }
