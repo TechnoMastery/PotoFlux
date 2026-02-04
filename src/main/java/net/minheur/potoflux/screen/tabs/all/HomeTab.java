7d88a6ca8db8b2996b7ea6f80f75da21b643fe96
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-04 17:54:14 +0100

    added Javadoc to the tabs

--- src/main/java/net/minheur/potoflux/screen/tabs/all/HomeTab.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/all/HomeTab.java
@@ -8,13 +8,23 @@ import net.minheur.potoflux.translations.Translations;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Tab class for the home tab
+ */
 public class HomeTab extends BaseTab {
+    /**
+     * This is the actual method to set the panel.<br>
+     * The overriding class will have to use this to add content to the {@link #PANEL}.
+     */
     @Override
     protected void setPanel() {
         addDesc();
         addVersion();
     }
 
+    /**
+     * Adds the description of the app
+     */
     private void addDesc() {
         JLabel desc = new JLabel(Translations.get("potoflux:tabs.home.credit"));
         desc.setFont(new Font("Consolas", Font.PLAIN, 15));
@@ -22,6 +32,9 @@ public class HomeTab extends BaseTab {
         PANEL.add(desc);
     }
 
+    /**
+     * Adds the version of the app
+     */
     private void addVersion() {
         String name = Functions.formatMessage(Translations.get("potoflux:tabs.home.version"),
                 PotoFlux.getVersion());
@@ -32,6 +45,10 @@ public class HomeTab extends BaseTab {
         PANEL.add(version);
     }
 
+    /**
+     * Used to set the title.
+     * @return the title of the tab.
+     */
     @Override
     protected String getTitle() {
         return Translations.get("potoflux:tabs.home.title");
--- src/main/java/net/minheur/potoflux/screen/tabs/all/TerminalTab.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/all/TerminalTab.java
@@ -4,28 +4,44 @@ import net.minheur.potoflux.screen.tabs.BaseTab;
 import net.minheur.potoflux.terminal.Terminal;
 import net.minheur.potoflux.translations.Translations;
 
+/**
+ * The tab that contains the terminal of potoflux.
+ */
 public class TerminalTab extends BaseTab {
+    /**
+     * Actual var containing the terminal
+     */
     private Terminal terminal;
 
+    /**
+     * This is the actual method to set the panel.<br>
+     * The overriding class will have to use this to add content to the {@link #PANEL}.
+     */
     @Override
     protected void setPanel() {
         addTerminal();
     }
 
+    /**
+     * Disable the preset
+     * @return false
+     */
     @Override
     protected boolean doPreset() {
         return false;
     }
 
+    /**
+     * Adds the terminal to the {@link #PANEL}
+     */
     private void addTerminal() {
         terminal = new Terminal(PANEL);
     }
 
-    @Override
-    protected String getTitle() {
-        return Translations.get("potoflux:tabs.term.title");
-    }
-
+    /**
+     * Getter for the {@link #terminal}
+     * @return the {@link #terminal}
+     */
     public Terminal getTerminal() {
         return terminal;
     }
