6710dfd256d42979a521955e4595636ca33ce08d
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-04 17:41:09 +0100

    added Javadoc for the tab system

--- src/main/java/net/minheur/potoflux/screen/tabs/Tab.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/Tab.java
@@ -3,8 +3,16 @@ package net.minheur.potoflux.screen.tabs;
 import net.minheur.potoflux.registry.IRegistryType;
 import net.minheur.potoflux.utils.ressourcelocation.ResourceLocation;
 
+/**
+ * A registry item to make tabs
+ * @param id the ID of your tab
+ * @param name the name of your tab (shown in the tab list)
+ * @param tabClass the class of the tab, used to actually get the panel
+ */
 public record Tab(ResourceLocation id, String name, Class<? extends BaseTab> tabClass) implements IRegistryType {
-
+/**
+ * Creates an instance of the tab class ({@link #tabClass})
+ */
     public BaseTab createInstance() {
         try {
             return tabClass.getDeclaredConstructor().newInstance();
