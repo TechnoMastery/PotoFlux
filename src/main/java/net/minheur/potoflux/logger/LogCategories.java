8b340b937a454d27e968c0d020519ec920b5bb20
Minheur4000 <ordi2526883@educ.normandie.fr>
2026-02-04 12:31:38 +0100

    added Javadoc to the log system

--- src/main/java/net/minheur/potoflux/logger/LogCategories.java
+++ src/main/java/net/minheur/potoflux/logger/LogCategories.java
@@ -1,19 +1,51 @@
 package net.minheur.potoflux.logger;
 
+/**
+ * Enum containing all potoflux log categories
+ */
 public enum LogCategories implements ILogCategory {
+    /**
+     * All logs talking about the potoflux terminal
+     */
     TERMINAL("terminal"),
+    /**
+     * All logs about the translation system
+     */
     TRANSLATIONS("translations"),
+    /**
+     * All logs related to loading mods
+     */
     MOD_LOADER("modLoader"),
+    /**
+     * All logs about the mod catalog
+     */
     CATALOG("modCatalog"),
+    /**
+     * All logs about the main screen system
+     */
     SCREEN("screen"),
+    /**
+     * All logs about the theme
+     */
     THEME("theme");
 
+    /**
+     * The actual String to be printed in the log
+     */
     private final String code;
 
+    /**
+     * Registers a log category
+     * @param code the code of the category
+     */
     LogCategories(String code) {
         this.code = code;
     }
 
+    /**
+     * Getter for the code
+     * @return the category's code
+     */
     @Override
     public String code() {
         return code;
--- src/main/java/net/minheur/potoflux/logger/PtfLogger.java
+++ src/main/java/net/minheur/potoflux/logger/PtfLogger.java
@@ -3,11 +3,24 @@ package net.minheur.potoflux.logger;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class used to log in potoflux.
+ */
 public final class PtfLogger {
+    /**
+     * Formatter used to print the time in the logs
+     */
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
+    /**
+     * Method to get the prefix of the command.<br>
+     * It contains the time, and all the components given in args. They are put in between {@code []}
+     * @param data the list of components to add to the prefix
+     * @return the built prefix
+     */
     public static String buildPrefix(List<String> data) {
         LocalTime now = LocalTime.now();
         String time = now.format(formatter);
@@ -18,11 +31,22 @@ public final class PtfLogger {
         for (String s : data) sb.append("[").append(s).append("]");
         return sb.toString();
     }
+    /**
+     * Builder retuning {@link #buildPrefix(List)}, but with the categories as a vararg.
+     * @param data vararg of the categories
+     * @return the built prefix
+     */
     public static String buildPrefix(String... data) {
-        if (data.length < 1) return "";
-        return buildPrefix(data);
+        return buildPrefix(Arrays.stream(data).toList());
     }
 
+    /**
+     * Builds the message with a main category
+     * @param category main category of the message
+     * @param message content of the log
+     * @param more adds more categories to the log
+     * @return the message
+     */
     public static String buildWithCategory(String category, String message, List<String> more) {
         List<String> data = new ArrayList<>();
         data.add(category);
@@ -32,9 +56,20 @@ public final class PtfLogger {
     }
 
     // actual methods
+    /**
+     * Print an info log message, with a content and optional categories
+     * @param message content of the log
+     * @param extraCategories optional categories of the log
+     */
     public static void info(String message, String... extraCategories) {
         System.out.println(buildWithCategory("INFO", message, List.of(extraCategories)));
     }
+    /**
+     * Print an info log message, with a content, a main log category (in the form of an {@link ILogCategory}) and optional categories
+     * @param message content of the log
+     * @param category main category of the log, should be a {@link ILogCategory}
+     * @param extraCategories optional categories of the log
+     */
     public static void info(String message, ILogCategory category, String... extraCategories) {
         List<String> allCategories = new ArrayList<>();
 
@@ -44,9 +79,20 @@ public final class PtfLogger {
         System.out.println(buildWithCategory("INFO", message, allCategories));
     }
 
+    /**
+     * Print a warning log message, with a content and optional categories
+     * @param message content of the log
+     * @param extraCategories optional categories of the log
+     */
     public static void warning(String message, String... extraCategories) {
         System.err.println(buildWithCategory("WARNING", message, List.of(extraCategories)));
     }
+    /**
+     * Print a warning log message, with a content, a main log category (in the form of an {@link ILogCategory}) and optional categories
+     * @param message content of the log
+     * @param category main category of the log, should be a {@link ILogCategory}
+     * @param extraCategories optional categories of the log
+     */
     public static void warning(String message, ILogCategory category, String... extraCategories) {
         List<String> allCategories = new ArrayList<>();
 
@@ -56,9 +102,20 @@ public final class PtfLogger {
         System.err.println(buildWithCategory("WARNING", message, allCategories));
     }
 
+    /**
+     * Print an error log message, with a content and optional categories
+     * @param message content of the log
+     * @param extraCategories optional categories of the log
+     */
     public static void error(String message, String... extraCategories) {
         System.err.println(buildWithCategory("ERROR", message, List.of(extraCategories)));
     }
+    /**
+     * Print an error log message, with a content, a main log category (in the form of an {@link ILogCategory}) and optional categories
+     * @param message content of the log
+     * @param category main category of the log, should be a {@link ILogCategory}
+     * @param extraCategories optional categories of the log
+     */
     public static void error(String message, ILogCategory category, String... extraCategories) {
         List<String> allCategories = new ArrayList<>();
 
