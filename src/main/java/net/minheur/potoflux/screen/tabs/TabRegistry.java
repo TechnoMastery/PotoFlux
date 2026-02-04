ab881b1a7d4b1883fa4538d746308608c5f89809
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-04 16:36:41 +0100

    started adding Javadoc to tabs registries

--- src/main/java/net/minheur/potoflux/screen/tabs/TabRegistry.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/TabRegistry.java
@@ -7,13 +7,29 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Registry to stores all tabs.<br>
+ * All tabs should be added to it, otherwise they will not be found by the app.
+ */
 public class TabRegistry implements IRegistry<Tab> {
+    /**
+     * Actual registry for the tabs, by their ID and their tabs.
+     */
     private static final Map<ResourceLocation, Tab> REGISTRY = new LinkedHashMap<>();
 
+    /**
+     * Getter for all the tabs
+     * @return all the tabs
+     */
     public static Collection<Tab> getAll() {
         return REGISTRY.values();
     }
 
+    /**
+     * Adds a tab to the reg
+     * @param item tab to add to the reg
+     * @return the added item
+     */
     @Override
     public Tab add(Tab item) {
         if (REGISTRY.containsKey(item.id())) throw new IllegalArgumentException("This tab is already added: " + item.id());
--- src/main/java/net/minheur/potoflux/screen/tabs/Tabs.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/Tabs.java
@@ -8,24 +8,56 @@ import net.minheur.potoflux.translations.Translations;
 
 import static net.minheur.potoflux.PotoFlux.fromModId;
 
+/**
+ * Registry of the potoflux tabs
+ */
 public class Tabs {
+    /**
+     * Registry list of the tabs
+     */
     private final RegistryList<Tab> LIST = new RegistryList<>();
+    /**
+     * Checks if the list has already been added to the reg.
+     */
     private static boolean hasGenerated = false;
 
+    /**
+     * The instance of this class, used to keep a link to the tab def
+     */
     public static Tabs INSTANCE;
 
+    /**
+     * Private builder, used to check that the tabs are not added twice to the reg.
+     */
     private Tabs() {
         if (hasGenerated) throw new IllegalStateException("Can't create the registry 2 times !");
         hasGenerated = true;
     }
 
+    /**
+     * The item of the home tab ({@link HomeTab}).
+     */
     public final Tab HOME = LIST.add(new Tab(fromModId("home"), Translations.get("potoflux:tabs.home.name"), HomeTab.class));
+    /**
+     * The item of the terminal tab ({@link TerminalTab}).
+     */
     public final Tab TERMINAL = LIST.add(new Tab(fromModId("terminal"), Translations.get("potoflux:tabs.term.name"), TerminalTab.class));
+    /**
+     * The item of the settings tab ({@link SettingsTab}).
+     */
     public final Tab SETTINGS = LIST.add(new Tab(fromModId("settings"), Translations.get("potoflux:tabs.settings.title"), SettingsTab.class));
+    /**
+     * The item of the catalog tab ({@link CatalogTab}).
+     */
     public final Tab CATALOG =
             Boolean.parseBoolean(PotoFluxLoadingContext.getOptionalFeatures().getProperty("catalogTab", "false")) ?
             LIST.add(new Tab(fromModId("catalog"), Translations.get("potoflux:tabs.catalog.name"), CatalogTab.class)) : null;
 
+    /**
+     * Create the instance of the class, then add the tabs to the events.<br>
+     * This is called on the tab register event.
+     * @param event the event to register to
+     */
     public static void register(RegisterTabsEvent event) {
         INSTANCE = new Tabs();
 
