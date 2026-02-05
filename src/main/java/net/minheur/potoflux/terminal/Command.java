737e3dfb88b77590ffe9b0f03f960622142aef95
Minheur4000 <ordi2526883@educ.normandie.fr>
2026-02-05 12:57:19 +0100

    convert Command class to record

--- src/main/java/net/minheur/potoflux/terminal/Command.java
+++ src/main/java/net/minheur/potoflux/terminal/Command.java
@@ -6,50 +6,15 @@ import net.minheur.potoflux.utils.ressourcelocation.ResourceLocation;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class Command implements IRegistryType {
-    private final ResourceLocation id;
-    private final String key;
-    private final Consumer<List<String>> commandOutput;
-    private final String commandHelp;
-    private final boolean hidden;
-
-    public Command(ResourceLocation id, String key, Consumer<List<String>> commandOutput, String commandHelp, boolean hidden) {
-        this.id = id;
-        this.key = key;
-        this.commandOutput = commandOutput;
-        this.commandHelp = commandHelp;
-        this.hidden = hidden;
-    }
+public record Command(ResourceLocation id, String key, Consumer<List<String>> commandOutput, String commandHelp,
+                      boolean hidden) implements IRegistryType {
     public Command(ResourceLocation id, String key, Consumer<List<String>> commandOutput, String commandHelp) {
-        this.id = id;
-        this.key = key;
-        this.commandOutput = commandOutput;
-        this.commandHelp = commandHelp;
 
-        this.hidden = false;
+        this(id, key, commandOutput, commandHelp, false);
     }
+
     public Command(ResourceLocation id, String key, Consumer<List<String>> commandOutput) {
-        this.commandOutput = commandOutput;
-        this.key = key;
-        this.id = id;
 
-        this.commandHelp = null;
-        this.hidden = true;
-    }
-
-    public ResourceLocation getId() {
-        return id;
-    }
-    public String getKey() {
-        return key;
-    }
-    public Consumer<List<String>> getCommandOutput() {
-        return commandOutput;
-    }
-    public String getCommandHelp() {
-        return commandHelp;
-    }
-    public boolean isHidden() {
-        return hidden;
+        this(id, key, commandOutput, null, true);
     }
 }
