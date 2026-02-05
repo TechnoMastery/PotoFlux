47d7c79d37dcb58d3e369c549e2830230746e926
Minheur4000 <ordi2526883@educ.normandie.fr>
2026-02-05 13:26:49 +0100

    added Javadoc to the commands

--- src/main/java/net/minheur/potoflux/terminal/commands/CommandHelp.java
+++ src/main/java/net/minheur/potoflux/terminal/commands/CommandHelp.java
@@ -2,52 +2,105 @@ package net.minheur.potoflux.terminal.commands;
 
 import net.minheur.potoflux.translations.Translations;
 
+/**
+ * The class containing all potoflux command helps
+ */
 public class CommandHelp {
+    /**
+     * A contant for the tabulation
+     */
     private static final String tab = "    ";
+    /**
+     * A constant for a tabulation, preceded by a new line.
+     */
     private static final String tabLine = "\n" + tab;
 
+    /**
+     * The help for the command {@code hello}.
+     * @return the help for {@code hello}.
+     */
     static String helloWorld() {
         return Translations.get("potoflux:command.hello_world") + tabLine +
                 Translations.get("potoflux:command.hello_world.use");
     }
+    /**
+     * The help for the command {@code clear}.
+     * @return the help for {@code clear}.
+     */
     static String clear() {
         return Translations.get("potoflux:command.clear") + tabLine +
                 Translations.get("potoflux:command.clear.use");
     }
+    /**
+     * The help for the command {@code time}.
+     * @return the help for {@code time}.
+     */
     static String time() {
         return Translations.get("potoflux:command.time") + tabLine +
                 Translations.get("potoflux:command.time.use");
     }
+    /**
+     * The help for the command {@code help}.
+     * @return the help for {@code help}.
+     */
     static String help() {
         return Translations.get("potoflux:command.help") + tabLine +
                 Translations.get("potoflux:command.help.use") + tabLine +
                 Translations.get("potoflux:command.help.use.command");
     }
+    /**
+     * The help for the command {@code echo}.
+     * @return the help for {@code echo}.
+     */
     static String echo() {
         return Translations.get("potoflux:command.echo") + tabLine +
                 Translations.get("potoflux:command.echo.use");
     }
+    /**
+     * The help for the command {@code tab}.
+     * @return the help for {@code tab}.
+     */
     static String tab() {
         return Translations.get("potoflux:command.tab") + tabLine +
                 Translations.get("potoflux:command.tab.use");
     }
+    /**
+     * The help for the command {@code source}.
+     * @return the help for {@code source}.
+     */
     static String source() {
         return Translations.get("potoflux:command.source") + tabLine +
                 Translations.get("potoflux:command.source.use");
     }
+    /**
+     * The help for the command {@code ascii}.
+     * @return the help for {@code ascii}.
+     */
     static String ascii() {
         return Translations.get("potoflux:command.ascii") + tabLine +
                 Translations.get("potoflux:command.ascii.use") + tabLine +
                 Translations.get("potoflux:command.ascii.use.choose");
     }
+    /**
+     * The help for the command {@code quit}.
+     * @return the help for {@code quit}.
+     */
     static String quit() {
         return Translations.get("potoflux:command.quit") + tabLine +
                 Translations.get("potoflux:command.quit.use");
     }
+    /**
+     * The help for the command {@code modList}.
+     * @return the help for {@code modList}.
+     */
     static String modList() {
         return Translations.get("potoflux:command.modList") + tabLine +
                 Translations.get("potoflux:command.modList.use");
     }
+    /**
+     * The help for the command {@code modDir}.
+     * @return the help for {@code modDir}.
+     */
     static String modDir() {
         return Translations.get("potoflux:command.modDir") + tabLine +
                 Translations.get("potoflux:command.modDir.use");
--- src/main/java/net/minheur/potoflux/terminal/commands/Commands.java
+++ src/main/java/net/minheur/potoflux/terminal/commands/Commands.java
@@ -6,33 +6,93 @@ import net.minheur.potoflux.terminal.Command;
 
 import static net.minheur.potoflux.PotoFlux.fromModId;
 
+/**
+ * Registry of all commands of potoflux
+ */
 public class Commands {
+    /**
+     * The registry list of all potoflux commands
+     */
     private final RegistryList<Command> LIST = new RegistryList<>();
+    /**
+     * Checks if the reg has already generated
+     */
     private static boolean hasGenerated = false;
 
+    /**
+     * The only instance of the reg that should exist
+     */
     public static Commands INSTANCE;
 
+    /**
+     * Builder to check of the reg has already generated
+     */
     public Commands() {
         if (hasGenerated) throw new IllegalStateException("Can't create the registry 2 times !");
         hasGenerated = true;
     }
 
+    /**
+     * Command for the {@code hello} command.
+     */
     public final Command HELLO_WORLD = LIST.add(new Command(fromModId("hello_world"), "hello", CommandActions::helloWorld, CommandHelp.help()));
+        /**
+     * Command for the {@code clear} command.
+     */
     public final Command CLEAR = LIST.add(new Command(fromModId("clear"), "clear", CommandActions::clear, CommandHelp.clear()));
+        /**
+     * Command for the {@code modList} command.
+     */
     public final Command MOD_LIST = LIST.add(new Command(fromModId("mod_list"), "modList", CommandActions::modList, CommandHelp.modList()));
+        /**
+     * Command for the {@code modDir} command.
+     */
     public final Command MOD_DIR = LIST.add(new Command(fromModId("mod_dir"), "modDir", CommandActions::modDir, CommandHelp.modDir()));
+        /**
+     * Command for the {@code time} command.
+     */
     public final Command TIME = LIST.add(new Command(fromModId("time"), "time", CommandActions::time, CommandHelp.time()));
+        /**
+     * Command for the {@code help} command.
+     */
     public final Command HELP = LIST.add(new Command(fromModId("help"), "help", CommandActions::help, CommandHelp.help()));
+        /**
+     * Command for the {@code echo} command.
+     */
     public final Command ECHO = LIST.add(new Command(fromModId("echo"), "echo", CommandActions::echo, CommandHelp.echo()));
+        /**
+     * Command for the {@code tab} command.
+     */
     public final Command TAB = LIST.add(new Command(fromModId("tab"), "tab", CommandActions::tab, CommandHelp.tab()));
+        /**
+     * Command for the {@code source} command.
+     */
     public final Command SOURCE = LIST.add(new Command(fromModId("source"), "source", CommandActions::sourceCode, CommandHelp.source()));
+        /**
+     * Command for the {@code ascii} command.
+     */
     public final Command ASCII = LIST.add(new Command(fromModId("ascii"), "ascii", CommandActions::writeAscii, CommandHelp.ascii()));
+        /**
+     * Command for the {@code quit} command.
+     */
     public final Command QUIT = LIST.add(new Command(fromModId("quit"), "quit", CommandActions::quit, CommandHelp.quit()));
 
     // hidden commands
+    /**
+     * Command for the {@code hide} command.<br>
+     * It's hidden
+     */
     public final Command HIDDEN = LIST.add(new Command(fromModId("hidden"), "hide", CommandActions::hidden));
+    /**
+     * Command for the {@code nope} command.<br>
+     * It's hidden
+     */
     public final Command NOPE = LIST.add(new Command(fromModId("nope"), "nope", CommandActions::nope));
 
+    /**
+     * Registers the commands to the reg
+     * @param event the event to register commands to
+     */
     public static void register(RegisterCommandsEvent event) {
         INSTANCE = new Commands();
         INSTANCE.LIST.register(event.reg);
