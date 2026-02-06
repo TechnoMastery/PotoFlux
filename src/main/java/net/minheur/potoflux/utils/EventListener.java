9915c0d22d9f8e4f2f5536907309c25679a7aba5
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-06 18:02:11 +0100

    patched wrong @Deprecated

--- src/main/java/net/minheur/potoflux/utils/EventListener.java
+++ src/main/java/net/minheur/potoflux/utils/EventListener.java
@@ -4,11 +4,9 @@ import java.io.Serializable;
 import java.util.function.Consumer;
 
 /**
- * Another way to add listeners to the mod Event bus.
- * @deprecated because another framework is easier to use
+ * Used to add event listeners to mod event bus, without telling explicitly the event
  * @param <E> The event to listen
  */
-@Deprecated(since = "6.4")
 @FunctionalInterface
 public interface EventListener<E> extends Consumer<E>, Serializable {
 }
