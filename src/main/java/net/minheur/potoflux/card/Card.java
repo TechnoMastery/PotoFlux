0ff75f32078fcff6f799fa4533fd5519d03066c5
Minheur2000 <adrien.rousselet@hotmail.com>
2026-02-06 19:02:58 +0100

    removed card learning stuff

--- src/main/java/net/minheur/potoflux/card/Card.java
+++
@@ -1,7 +0,0 @@
-package net.minheur.potoflux.card;
-
-@Deprecated(since = "6.4", forRemoval = true)
-public class Card {
-    public String main;
-    public String secondary;
-}
--- src/main/java/net/minheur/potoflux/card/CardList.java
+++
@@ -1,9 +0,0 @@
-package net.minheur.potoflux.card;
-
-import java.util.List;
-
-@Deprecated(since = "6.4", forRemoval = true)
-public class CardList {
-    public String name;
-    public List<Card> cards;
-}
