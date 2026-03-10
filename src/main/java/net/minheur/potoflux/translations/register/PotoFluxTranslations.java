074fdf095029978f591501a18760e9a9147a4bd5
Minheur4000 <adrien.rousselet@hotmail.com>
2026-03-10 13:27:15 +0100

    added PotoFluxTranslations

--- src/main/java/net/minheur/potoflux/translations/register/PotoFluxTranslations.java
+++ src/main/java/net/minheur/potoflux/translations/register/PotoFluxTranslations.java
@@ -23,73 +23,99 @@ public class PotoFluxTranslations extends AbstractTranslationsRegistry {
     protected void makeTranslation() {
         // commands
         addCommand(Commands.INSTANCE.ASCII)
-                .en("Show an ASCII");
+                .en("Show an ASCII")
+                .fr("Affiche un ASCII");
         addCommandUse(Commands.INSTANCE.ASCII)
-                .en("Usage: ascii");
+                .en("Usage: ascii")
+                .fr("Utilisation : ascii");
         addCommandUse(Commands.INSTANCE.ASCII,"choose")
-                .en("Usage: ascii <name>");
+                .en("Usage: ascii <name>")
+                .fr("Utilisation : ascii <nom>");
 
         addCommand(Commands.INSTANCE.CLEAR)
-                .en("Clears the terminal");
+                .en("Clears the terminal")
+                .fr("Vide le terminal");
         addCommandUse(Commands.INSTANCE.CLEAR)
-                .en("Usage: clear");
+                .en("Usage: clear")
+                .fr("Utilisation : clear");
 
         addCommand(Commands.INSTANCE.VERSION)
-                .en("Get actual PotoFlux version");
+                .en("Get actual PotoFlux version")
+                .fr("Affiche la version actuelle de PotoFLux");
         addCommandUse(Commands.INSTANCE.VERSION)
-                .en("Usage: version");
+                .en("Usage: version")
+                .fr("Utilisation : version");
 
         addCommand(Commands.INSTANCE.ECHO)
-                .en("Repeats what you give");
+                .en("Repeats what you give")
+                .fr("Répète ce que vous écrivez");
         addCommandUse(Commands.INSTANCE.ECHO)
-                .en("Usage: echo <text>");
+                .en("Usage: echo <text>")
+                .fr("Utilisation : echo <texte>");
 
         addCommand(Commands.INSTANCE.HELLO_WORLD)
-                .en("Says hello world");
+                .en("Says hello world")
+                .fr("Dit hello world");
         addCommandUse(Commands.INSTANCE.ECHO)
-                .en("Usage: hello");
+                .en("Usage: hello")
+                .fr("Utilisation : hello");
 
         addCommand(Commands.INSTANCE.HELP)
-                .en("Shows this menu");
+                .en("Shows this menu")
+                .fr("Affiche ce menu");
         addCommand(Commands.INSTANCE.HELP, "title")
-                .en("Command list:");
+                .en("Command list:")
+                .fr("Liste des commandes :");
         addCommandUse(Commands.INSTANCE.HELP)
-                .en("Usage: help");
+                .en("Usage: help")
+                .fr("Utilisation : help");
         addCommandUse(Commands.INSTANCE.HELP, "command")
-                .en("Usage: help <command>");
+                .en("Usage: help <command>")
+                .fr("Utilisation : help <commande>");
 
         addCommand(Commands.INSTANCE.HIDDEN, "out")
-                .en("Hidden command :)");
+                .en("Hidden command :)")
+                .fr("Commande caché ;)");
 
         addCommand(Commands.INSTANCE.NOPE, "out")
                 .en("I got you !")
                 .fr("CHEH");
 
         addCommand(Commands.INSTANCE.QUIT)
-                .en("Exits app");
+                .en("Exits app")
+                .fr("Ferme l'app");
         addCommand(Commands.INSTANCE.QUIT, "out")
-                .en("Exiting...");
+                .en("Exiting...")
+                .fr("Fermeture...");
         addCommandUse(Commands.INSTANCE.QUIT)
-                .en("Usage: quit");
+                .en("Usage: quit")
+                .fr("Utilisation : quit");
 
         addCommand(Commands.INSTANCE.SOURCE)
-                .en("Link to source code");
+                .en("Link to source code")
+                .fr("Lien vers le code source");
         addCommand(Commands.INSTANCE.SOURCE, "out")
-                .en("Opened code source in browser !");
+                .en("Opened code source in browser !")
+                .fr("Code source ouvert dans le navigateur !");
         addCommand(Commands.INSTANCE.SOURCE, "noBrowse")
-                .en("Could not open source code ! Here is the link:");
+                .en("Could not open source code ! Here is the link:")
+                .fr("Impossible d'ouvrir le code source dans le navigateur. Voici le lien :");
         addCommandUse(Commands.INSTANCE.SOURCE)
-                .en("Usage: source-code");
+                .en("Usage: source-code")
+                .fr("Utilisation : source-code");
 
         addCommand(Commands.INSTANCE.TAB)
-                .en("Open given tab");
+                .en("Open given tab")
+                .fr("Ouvre l'onglet donné");
         addCommand(Commands.INSTANCE.TAB, "null")
                 .en("Tab $$1 doesn't exists !")
                 .fr("L'onglet $$1 n'existe pas !");
         addCommand(Commands.INSTANCE.TAB, "opened")
-                .en("This tab is already opened !");
+                .en("This tab is already opened !")
+                .fr("Cet onglet est déjà ouvert !");
         addCommandUse(Commands.INSTANCE.TAB)
-                .en("Usage: tab <tabResourceLoc>");
+                .en("Usage: tab <tabResourceLoc>")
+                .fr("Utilisation : tab <ressourceLocDeLaTab>");
 
         addCommand(Commands.INSTANCE.TAB_LIST)
                 .en("Lists all the tabs")
@@ -105,39 +131,54 @@ public class PotoFluxTranslations extends AbstractTranslationsRegistry {
                 .fr("Utilisation : tabList --resourceLoc");
 
         addCommand(Commands.INSTANCE.TIME)
-                .en("Tells the time");
+                .en("Tells the time")
+                .fr("Dit le temps (très précis)");
         addCommandUse(Commands.INSTANCE.TIME)
-                .en("Usage: time");
+                .en("Usage: time")
+                .fr("Utilisation : time");
 
         addCommand(Commands.INSTANCE.MOD_LIST)
-                .en("Lists you loaded mods");
+                .en("Lists you loaded mods")
+                .fr("Liste tous les mods chargé (activés)");
         addCommandUse(Commands.INSTANCE.MOD_LIST)
-                .en("Usage: modList");
+                .en("Usage: modList")
+                .fr("Utilisation : modList");
 
         addCommand(Commands.INSTANCE.MOD_DIR)
-                .en("Open mod directory");
+                .en("Open mod directory")
+                .fr("Ouvre le dossier des mods");
         addCommandUse(Commands.INSTANCE.MOD_DIR)
-                .en("Usage: modDir");
+                .en("Usage: modDir")
+                .fr("Utilisation : modDir");
 
         addCommandPro("empty")
-                .en("Could not execute empty command !");
+                .en("Could not execute empty command !")
+                .fr("Impossible d'exécuter une commande vide !");
         addCommandPro("none")
-                .en("Command not recognized ! Try \"help\" !");
+                .en("Command not recognized ! Try \"help\" !")
+                .fr("Commande non reconnu ! Essayez \"help\" !");
 
         addPref("reload")
-                .en("Please restart PotoFlux to apply.");
+                .en("Please restart PotoFlux to apply.")
+                .fr("Merci de redémarrer PotoFlux, afin d'appliquer.");
         addPref("ascii")
-                .en("ASCII");
+                .en("ASCII")
+                .fr("ASCII");
         addPref("ascii", "select")
-                .en("Select terminal ASCII");
+                .en("Select terminal ASCII")
+                .fr("Choisissez un ASCII du terminal");
         addPref("ascii", "button")
-                .en("change terminal ASCII");
+                .en("Change terminal ASCII")
+                .fr("Changer l'ASCII du terminal");
         addPref("lang")
-                .en("Language");
+                .en("Language")
+                .fr("Langue");
         addPref("lang", "select")
-                .en("Select language");
+                .en("Select language")
+                .fr("Sélectionnez la langue");
         addPref("lang", "button")
-                .en("Change language");
+                .en("Change language")
+                .fr("Changer la langue");
         addPref("theme")
                 .en("Theme")
                 .fr("Thème");
@@ -149,7 +190,8 @@ public class PotoFluxTranslations extends AbstractTranslationsRegistry {
                 .fr("Changer le thème");
 
         addScreen("tabHereNotHere")
-                .en("ERROR: this tab is detected but unexisting !");
+                .en("ERROR: this tab is detected but unexisting !")
+                .fr("Erreur : cette onglet est détecté mais inéxistante !");
 
         addHomeTab("credit")
                 .en("Created by Min_heur2000 - TechnoMastery")
@@ -165,10 +207,12 @@ public class PotoFluxTranslations extends AbstractTranslationsRegistry {
                 .fr("PotoFlux en version $$1");
 
         addSettingsTab("name")
-                .en("Settings");
+                .en("Settings")
+                .fr("Paramètres");
 
         addTerminalTab("name")
-                .en("Terminal");
+                .en("Terminal")
+                .fr("Terminal");
 
         addCatalogTab("name")
                 .en("Catalog")
