187d9672724e10dffcb94b48a7eff297ab138ab8
Minheur4000 <adrien.rousselet@hotmail.com>
2026-03-10 13:14:18 +0100

    added FileTranslations

--- src/main/java/net/minheur/potoflux/translations/register/FileTranslations.java
+++ src/main/java/net/minheur/potoflux/translations/register/FileTranslations.java
@@ -16,29 +16,40 @@ public class FileTranslations extends AbstractTranslationsRegistry {
     @Override
     protected void makeTranslation() {
         error("exist")
-                .en("File already existing.");
+                .en("File already exists.")
+                .fr("Le fichier existe déjà");
         error("exist", "desc")
-                .en("A file with the same name already exists.");
+                .en("A file with the same name already exists.")
+                .fr("Un fichier de même nom existe déjà.");
         error("saving")
-                .en("Error while saving file: ");
+                .en("Error while saving file: ")
+                .fr("Erreur lors de l'enregistrement du fichier : ");
         error("not_found")
-                .en("File not found");
+                .en("File not found")
+                .fr("Fichier non trouvé");
         error("not_found", "linked")
-                .en("File not found: ");
+                .en("File not found: ")
+                .fr("fichier non trouvé : ");
 
         add("loaded")
-                .en("File loaded: ");
+                .en("File loaded: ")
+                .fr("Fichier chargé : ");
         add("saved")
-                .en("File saved");
+                .en("File saved")
+                .fr("Fichier sauvegardé");
 
         json()
-                .en("JSON files (*.json)");
+                .en("JSON files (*.json)")
+                .fr("Fichiers JSON (*.json)");
         json("error", "invalid")
-                .en("The JSON file is invalid !");
+                .en("The JSON file is invalid !")
+                .fr("Ce fichier JSON est invalide !");
         json("error", "loading")
-                .en("Error while loading JSON file !");
+                .en("Error while loading JSON file !")
+                .fr("Erreur lors du chargement du fichier JSON !");
         json("import")
-                .en("Import JSON file...");
+                .en("Import JSON file...")
+                .fr("Importez un  fichier JSON...");
     }
 
     /**
