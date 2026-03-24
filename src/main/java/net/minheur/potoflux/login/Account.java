19c7b6441ae89f3a7d24ec51d7b2a070d66d3dbd
Minheur2000 <adrien.rousselet@hotmail.com>
2026-03-24 18:42:35 +0100

    added Account

---
+++ src/main/java/net/minheur/potoflux/login/Account.java
@@ -0,0 +1,11 @@
+package net.minheur.potoflux.login;
+
+public class Account {
+
+    public String uuid;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String[] perms;
+
+}
