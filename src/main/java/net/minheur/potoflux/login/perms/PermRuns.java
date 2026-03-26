20c354bbcb81df489acdfe89a71c9d2fc6dec9d9
Minheur4000 <adrien.rousselet@hotmail.com>
2026-03-26 12:54:26 +0100

    started getting data

--- src/main/java/net/minheur/potoflux/login/perms/PermRuns.java
+++ src/main/java/net/minheur/potoflux/login/perms/PermRuns.java
@@ -4,10 +4,19 @@ import net.minheur.potoflux.PotoFlux;
 import net.minheur.potoflux.ui.AddUserDialog;
 
 import javax.swing.*;
+import java.util.List;
 
 public class PermRuns {
     static void addUser() {
-        JDialog dialog = new AddUserDialog(PotoFlux.app.getFrame());
+        AddUserDialog dialog = new AddUserDialog(PotoFlux.app.getFrame());
         dialog.setVisible(true);
+
+        if (!dialog.isConfirmed()) return;
+
+        String email = dialog.getEmail();
+        String password = dialog.getPassword();
+        String firstName = dialog.getFirstName();
+        String lastName = dialog.getLastName();
+        List<Perms> perms = dialog.getSelectedPerms();
     }
 }
