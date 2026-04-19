2ce587e27ffe40c98fe9e00b54677eaa11736e5a
Minheur2000 <adrien.rousselet@hotmail.com>
2026-04-19 14:57:24 +0200

    added response for 'is account creation enabled'

---
+++ src/main/java/net/minheur/potoflux/login/response/IsAccountCreationEnabledResponse.java
@@ -0,0 +1,8 @@
+package net.minheur.potoflux.login.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class IsAccountCreationEnabledResponse {
+    @SerializedName("is_enabled")
+    public boolean isEnabled;
+}
