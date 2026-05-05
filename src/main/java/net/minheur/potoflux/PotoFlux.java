1590f20713da1474724f6ab3296195ebd0a1f146
Minheur4000 <adrien.rousselet@hotmail.com>
2026-05-05 13:09:25 +0200

    added & implemented FXLoadingScreen

--- src/main/java/net/minheur/potoflux/PotoFlux.java
+++ src/main/java/net/minheur/potoflux/PotoFlux.java
@@ -17,8 +17,8 @@ import net.minheur.potoflux.loader.mod.events.RegisterRunsEvent;
 import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import net.minheur.potoflux.logger.LogSaver;
 import net.minheur.potoflux.loader.mod.events.*;
+import net.minheur.potoflux.screen.FXLoadingScreen;
 import net.minheur.potoflux.screen.PotoScreen;
-import net.minheur.potoflux.screen.LoadingScreen;
 import net.minheur.potoflux.screen.menu.MenuContent;
 import net.minheur.potoflux.screen.tabs.Tabs;
 import net.minheur.potoflux.terminal.commands.Commands;
@@ -68,36 +68,37 @@ public class PotoFlux {
         });
         Platform.startup(() -> {});
 
-        LoadingScreen startScreen = new LoadingScreen();
-        startScreen.setVisible(true);
+        FXLoadingScreen startScreen = new FXLoadingScreen();
+        Platform.runLater(startScreen::setup);
+        Platform.runLater(startScreen::show);
 
         // env setup
-        startScreen.updateStage("Loading environment...");
+        Platform.runLater(() -> startScreen.updateStage("Loading environment..."));
         if (args.length < 1) PotoFluxLoadingContext.setDevEnv(false);
         else PotoFluxLoadingContext.setDevEnv(args[0].equals("devEnv"));
 
         // important inits
-        startScreen.updateStage("Init...");
+        Platform.runLater(() -> startScreen.updateStage("Init..."));
         LogSaver.init();
         LogAmountManager.init();
 
         if (PotoFluxLoadingContext.isDevEnv()) PtfLogger.info("App running in dev env !");
 
         // app version
-        startScreen.updateStage("Getting version...");
+        Platform.runLater(() -> startScreen.updateStage("Getting version..."));
         String version = getVersion();
         if (version != null) PtfLogger.info("Running potoflux v" + version);
 
         // load optional features
-        startScreen.updateStage("Loading features...");
+        Platform.runLater(() -> startScreen.updateStage("Loading features..."));
         PotoFluxLoadingContext.loadFeatures();
 
         // enable or not log saving
-        startScreen.updateStage("Loading log logic...");
+        Platform.runLater(() -> startScreen.updateStage("Loading log logic..."));
         runLogSavingEnablingLogic();
 
         // set theme
-        startScreen.updateStage("Getting the theme...");
+        Platform.runLater(() -> startScreen.updateStage("Getting the theme..."));
         String theme = UserPrefsManager.getTheme();
         if (theme.equals("dark")) FlatDarkLaf.setup(); // dark theme
         else if (theme.equals("light")) FlatLightLaf.setup(); // light theme
@@ -105,12 +106,12 @@ public class PotoFlux {
         PtfLogger.info("Theme set to " + theme);
 
         // load translations
-        startScreen.updateStage("Loading translations...");
+        Platform.runLater(() -> startScreen.updateStage("Loading translations..."));
         if (!UserPrefsManager.getUserLang().equals("en"))
             Translations.load(UserPrefsManager.getUserLang());
 
         // def modEventBus
-        startScreen.updateStage("Loading event bus...");
+        Platform.runLater(() -> startScreen.updateStage("Loading event bus..."));
         ModEventBus bus = PotoFluxLoadingContext.get().getModEventBus();
 
         // subscribe PotoFlux's data to modEventBus
@@ -121,12 +122,12 @@ public class PotoFlux {
         bus.addListener(MenuContent::register);
 
         // load all addons
-        startScreen.updateStage("Loading addons...");
+        Platform.runLater(() -> startScreen.updateStage("Loading addons..."));
         new AddonLoader().loadAddons();
         PotoFluxLoadingContext.loadMods();
 
         // post all registrations
-        startScreen.updateStage("Registering data...");
+        Platform.runLater(() -> startScreen.updateStage("Registering data..."));
 
         try {
             bus.post(new RegisterLangEvent()); // register lang BEFORE anything else
@@ -140,14 +141,14 @@ public class PotoFlux {
         }
 
         // run all start logic runs
-        startScreen.updateStage("Running start logic...");
+        Platform.runLater(() -> startScreen.updateStage("Running start logic..."));
         for (ActionRun ar : StartLogicRunRegistry.getAll()) ar.run().run();
 
         // invoke app (start)
-        startScreen.updateStage("Launching app...");
+        Platform.runLater(() -> startScreen.updateStage("Launching app..."));
         SwingUtilities.invokeLater(() -> {
             app = new PotoScreen();
-            startScreen.close();
+            Platform.runLater(startScreen::close);
 
             // run all start ui runs
             for (ActionRun ar : StartUiRunRegistry.getAll()) ar.run().run();
---
+++ src/main/java/net/minheur/potoflux/screen/FXLoadingScreen.java
@@ -0,0 +1,45 @@
+package net.minheur.potoflux.screen;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class FXLoadingScreen {
+
+    private Stage stage;
+    private Label label;
+    private Label stageLabel;
+
+    public void setup() {
+
+        stage = new Stage();
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Loading Potoflux...");
+
+        label = new Label("Potoflux is loading...");
+        stageLabel = new Label();
+
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-padding: 20;");
+
+        root.getChildren().addAll(label, stageLabel);
+
+        Scene scene = new Scene(root, 400, 200);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+    }
+
+    public void show() {
+        stage.show();
+    }
+    public void updateStage(String text) {
+        stageLabel.setText(text);
+    }
+    public void close() {
+        stage.close();
+    }
+}
