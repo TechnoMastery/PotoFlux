5ae51759bb951aa16ee92dec06c286bc3c51df12
Minheur4000 <adrien.rousselet@hotmail.com>
2026-05-07 13:19:13 +0200

    added UiUtils.showInputDialog (x3)

--- src/main/java/net/minheur/potoflux/ui/UiUtils.java
+++ src/main/java/net/minheur/potoflux/ui/UiUtils.java
@@ -1,13 +1,20 @@
 package net.minheur.potoflux.ui;
 
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import net.minheur.potoflux.PotoFlux;
 import net.minheur.potoflux.translations.Translations;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import net.minheur.potoflux.utils.SmartSupplier;
 
 import javax.annotation.CheckForNull;
 import javax.swing.*;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * A utility class for UI related
@@ -50,6 +57,62 @@ public final class UiUtils {
                 message,
                 Translations.get("common:info"),
                 JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+
+    public static <R> R showInputDialog(
+            @NotNull R[] options, int defaultIndex,
+            @Nullable String title,
+            @Nullable String header,
+            @Nullable String content,
+            @Nullable String iconDir, @Nullable String cssDir
+    ) {
+        ChoiceDialog<R> dialog = new ChoiceDialog<>(
+                options[defaultIndex], options
+        );
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.setContentText(content);
+
+        if (iconDir != null) {
+            Stage dialogStage = ((Stage) dialog.getDialogPane().getScene().getWindow());
+            dialogStage.getIcons().add(new Image(
+                    Objects.requireNonNull(UiUtils.class.getResourceAsStream(iconDir))
+            ));
+        }
+
+        if (cssDir != null)
+            dialog.getDialogPane().getStylesheets().add(
+                    UiUtils.class.getResource(cssDir).toExternalForm()
+            );
+
+        Optional<R> result = dialog.showAndWait();
+
+        return result.orElse(null);
+    }
+    public static <R> R showInputDialog(
+            @NotNull R[] options, int defaultIndex,
+            @Nullable String title,
+            @Nullable String header,
+            @Nullable String content,
+            String cssDir
+    ) {
+        return showInputDialog(
+                options, defaultIndex,
+                title, header, content,
+                null, cssDir
+        );
+    }
+    public static <R> R showInputDialog(
+            @NotNull R[] options, int defaultIndex,
+            @Nullable String title,
+            @Nullable String header,
+            @Nullable String content
+    ) {
+        return showInputDialog(
+                options, defaultIndex,
+                title, header, content,
+                null, null
         );
     }
 
