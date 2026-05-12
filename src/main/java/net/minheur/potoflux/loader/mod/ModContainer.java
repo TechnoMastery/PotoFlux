7f3e965ab2606676d89d62c31d0a36d842f1df65
Minheur4000 <adrien.rousselet@hotmail.com>
2026-05-12 15:17:17 +0200

    added constructor in ModContainer

--- src/main/java/net/minheur/potoflux/loader/mod/ModContainer.java
+++ src/main/java/net/minheur/potoflux/loader/mod/ModContainer.java
@@ -4,4 +4,9 @@ public class ModContainer {
     public Mod mod;
     public Class<?> clazz;
     public ModState state = ModState.NOT_PROCESSED;
+
+    public ModContainer(Mod mod, Class<?> clazz) {
+        this.mod = mod;
+        this.clazz = clazz;
+    }
 }
