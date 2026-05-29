1da0c3893a40b57da508789168f93d43b3886068
Minheur2000 <adrien.rousselet@hotmail.com>
2026-05-29 18:18:57 +0200

    added javadoc in token classes

--- src/main/java/net/minheur/potoflux/login/InvalidTokenException.java
+++ src/main/java/net/minheur/potoflux/login/InvalidTokenException.java
@@ -4,16 +4,32 @@ package net.minheur.potoflux.login;
  * This exception is thrown when your token is invalid, which means you maybe have modified it.
  */
 public class InvalidTokenException extends Exception {
+    /**
+     * Constructor with a simple message
+     * @param message of the exception
+     */
     public InvalidTokenException(String message) {
         super(message);
     }
 
+    /**
+     * Empty init
+     */
     public InvalidTokenException() {}
 
+    /**
+     * Constructor with a message and a cause
+     * @param message of the exception
+     * @param cause of the exception
+     */
     public InvalidTokenException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructor with only a cause
+     * @param cause of the exception
+     */
     public InvalidTokenException(Throwable cause) {
         super(cause);
     }
