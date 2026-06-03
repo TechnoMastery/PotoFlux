e1185fecd582cb3d9392457b60cfa32e493d332e
Minheur4000 <adrien.rousselet@hotmail.com>
2026-06-03 12:42:48 +0200

    added javadoc in dialog datas

--- src/main/java/net/minheur/potoflux/ui/dialogData/NewAccountData.java
+++ src/main/java/net/minheur/potoflux/ui/dialogData/NewAccountData.java
@@ -6,12 +6,33 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+/**
+ * Data for a new account
+ */
 public class NewAccountData {
+    /**
+     * Email of the new account
+     */
     public @NotNull String email;
+    /**
+     * First name of the user
+     */
     public @NotNull String firstName;
+    /**
+     * Last name of the user
+     */
     public @NotNull String lastName;
+    /**
+     * Password for the new account
+     */
     public @NotNull String password;
 
+    /**
+     * Rank for the new account
+     */
     public @Nullable Integer rank;
+    /**
+     * Perms granted to the new account
+     */
     public @Nullable List<Perms> perms;
 }
