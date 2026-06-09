a74ad9e662f0c1bb6aab9fada9393fba90e685a7
Minheur2000 <adrien.rousselet@hotmail.com>
2026-06-09 12:15:46 +0200

    added post for notifications

--- src/main/java/net/minheur/potoflux/login/notifications/Notification.java
+++ src/main/java/net/minheur/potoflux/login/notifications/Notification.java
@@ -1,4 +1,11 @@
 package net.minheur.potoflux.login.notifications;
 
-public record Notification() {
-}
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+
+public record Notification(
+        long id,
+        JsonObject message,
+        @SerializedName("created_at")
+        String timestamp
+) {}
