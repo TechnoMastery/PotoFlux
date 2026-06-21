5c1e3535dd14b8d8be08dda97efa62ec59d518d1
Minheur2000 <adrien.rousselet@hotmail.com>
2026-06-21 19:41:43 +0200

    added DEBUG tab

---
+++ src/main/java/net/minheur/potoflux/screen/tabs/all/DebugTab.java
@@ -0,0 +1,31 @@
+package net.minheur.potoflux.screen.tabs.all;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import net.minheur.potoflux.screen.tabs.BaseVTab;
+
+public class DebugTab extends BaseVTab<BorderPane> {
+    @Override
+    protected void instantiate() {
+        PANEL = new BorderPane();
+        vContent = new VBox();
+        PANEL.setCenter(vContent);
+        vContent.getChildren().add(new Label("DEBUG tab"));
+    }
+
+    @Override
+    protected void setPanel() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Debug";
+    }
+
+    @Override
+    protected boolean doPreset() {
+        return false;
+    }
+}
