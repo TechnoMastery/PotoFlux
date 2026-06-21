e83729df3aa04eb1c52fea904270497bb5364e60
Minheur2000 <adrien.rousselet@hotmail.com>
2026-06-21 20:50:16 +0200

    debug for system

--- src/main/java/net/minheur/potoflux/screen/tabs/all/DebugTab.java
+++ src/main/java/net/minheur/potoflux/screen/tabs/all/DebugTab.java
@@ -1,9 +1,14 @@
 package net.minheur.potoflux.screen.tabs.all;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import net.minheur.potoflux.logger.PtfLogger;
+import net.minheur.potoflux.login.RequestPoster;
 import net.minheur.potoflux.screen.tabs.BaseVTab;
+
+import java.io.IOException;
 
 public class DebugTab extends BaseVTab<BorderPane> {
     @Override
@@ -16,7 +21,16 @@ public class DebugTab extends BaseVTab<BorderPane> {
 
     @Override
     protected void setPanel() {
-
+        Button btn = new Button("MK ACCOUNT");
+        btn.setOnAction((event) -> {
+            try {
+                String content = RequestPoster.selfCreateAccount("adrien.rousselet@hotmail.com", "PotatoChef");
+                PtfLogger.info(content);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        vContent.getChildren().add(btn);
     }
 
     @Override
