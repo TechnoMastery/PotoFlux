2c79c228177c61b4d8970a9349a20d9ffa045157
Minheur2000 <adrien.rousselet@hotmail.com>
2026-07-10 18:24:46 +0200

    feat(logs): Added LogCategories.SETTINGS

--- src/main/java/net/minheur/potoflux/logger/LogCategories.java
+++ src/main/java/net/minheur/potoflux/logger/LogCategories.java
@@ -57,7 +57,8 @@ public enum LogCategories implements ILogCategory {
     /**
      * All logs about the theme
      */
-    THEME("theme");
+    THEME("theme"),
+    SETTINGS("settings");
 
     /**
      * The actual String to be printed in the log
